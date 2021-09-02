2c88e2134904f899603e45d3ee122d83b8ab5cf9
gouda-m <mohamed.abdelmawgood4@gmail.com>
2021-09-02 13:02:50 +0200

    new day

---
+++ include/constants.hpp
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS__H
+#define CONSTANTS__H
+
+#include <stdint.h>
+#include <array>
+
+
+#define MEMORY_SIZE 0xFFFF
+
+using addr_t = uint16_t;
+
+
+#endif // CONSTANTS__H
--- include/memory.hpp
+++ include/memory.hpp
@@ -1,10 +1,17 @@
-#ifndef CPU__H
-#define CPU__H
-#include <stdint.h>
+#ifndef MEMORY__H
+#define MEMORY__H
+
+#include "constants.hpp"
+
 
 class memory
 {
+private:
+    std::array<uint8_t, MEMORY_SIZE> mem{};
 
+public:
+    uint8_t read(addr_t adrr);
+    void write(addr_t adrr, uint8_t data);
 };
 
-#endif // CPU__H
+#endif // MEMORY__H
